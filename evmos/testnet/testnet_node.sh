5eb8fee6a3c8913b1104afc4ca4397ac2a3cb960
gregs <github@gregs.io>
2023-11-06 11:09:10 +0100

    Add support for Polkachu
    - Evmos testnet tutorial found https://polkachu.com/testnets/evmos/
       - Genesis file
       - State-Sync
       - Snapshot sync
    
    - Dockerfile update
    - No more need to edit Genesis file
    - Image bump to gregsaram/evmos:15.0.0-rc2-testnet
    - Config scrits nit

--- evmos/testnet/testnet_node.sh
+++ evmos/testnet/testnet_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CHAINID="${CHAIN_ID:-evmos_9000-4}"
-MONIKER="testnetvalidator"
+MONIKER="testnetvalidator-gregsio"
 # Remember to change to other types of keyring like 'file' in-case exposing to outside world,
 # otherwise your balance will be wiped quickly
 # The keyring test does not require private key to steal tokens from you
@@ -9,7 +9,6 @@ KEYRING="test"
 KEYALGO="eth_secp256k1"
 LOGLEVEL="info"
 # Set dedicated home directory for the evmosd instance
-#HOMEDIR="$HOME/evmosd"
 HOMEDIR="/evmos/evmosd"
 CONFIGDIR="$HOMEDIR/config"
 
@@ -29,21 +28,10 @@ command -v jq >/dev/null 2>&1 || {
 	exit 1
 }
 
-command -v unzip >/dev/null 2>&1 || {
-	echo >&2 "unzip not installed."
-	exit 1
-}
-
 command -v wget >/dev/null 2>&1 || {
 	echo >&2 "wget not installed."
 	exit 1
 }
-
-command -v sponge >/dev/null 2>&1 || {
-	echo >&2 "moreutils not installed."
-	exit 1
-}
-
 
 # used to exit on first error (any non-zero exit code)
 set -e
@@ -65,22 +53,12 @@ while [[ $# -gt 0 ]]; do
 		overwrite="n"
 		shift # Move past the argument
 		;;
-	--no-install)
-		echo "Flag --no-install passed -> Skipping installation of the evmosd binary."
-		install=false
-		shift # Move past the flag
-		;;
 	*)
 		echo "Unknown flag passed: $key -> Exiting script!"
 		exit 1
 		;;
 	esac
 done
-
-if [[ $install == true ]]; then
-	# (Re-)install daemon
-	make install
-fi
 
 # User prompt if neither -y nor -n was passed as a flag
 # and an existing local node configuration is found.
@@ -104,50 +82,23 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	evmosd config keyring-backend "$KEYRING" --home "$HOMEDIR"
 	evmosd config chain-id "$CHAINID" --home "$HOMEDIR"
 
-
 	# myKey address 0x7cb61d4117ae31a12e393a1cfa3bac666481d02e | evmos10jmp6sgh4cc6zt3e8gw05wavvejgr5pwjnpcky
 	VAL_KEY="mykey"
 	VAL_MNEMONIC="gesture inject test cycle original hollow east ridge hen combine junk child bacon zero hope comfort vacuum milk pitch cage oppose unhappy lunar seat"
 
-	# dev0 address 0xc6fe5d33615a1c52c08018c47e8bc53646a0e101 | evmos1cml96vmptgw99syqrrz8az79xer2pcgp84pdun
-	USER1_KEY="dev0"
-	USER1_MNEMONIC="copper push brief egg scan entry inform record adjust fossil boss egg comic alien upon aspect dry avoid interest fury window hint race symptom"
-
-	# dev1 address 0x963ebdf2e1f8db8707d05fc75bfeffba1b5bac17 | evmos1jcltmuhplrdcwp7stlr4hlhlhgd4htqh3a79sq
-	USER2_KEY="dev1"
-	USER2_MNEMONIC="maximum display century economy unlock van census kite error heart snow filter midnight usage egg venture cash kick motor survey drastic edge muffin visual"
-
-	# dev2 address 0x40a0cb1C63e026A81B55EE1308586E21eec1eFa9 | evmos1gzsvk8rruqn2sx64acfsskrwy8hvrmafqkaze8
-	USER3_KEY="dev2"
-	USER3_MNEMONIC="will wear settle write dance topic tape sea glory hotel oppose rebel client problem era video gossip glide during yard balance cancel file rose"
-
-	# dev3 address 0x498B5AeC5D439b733dC2F58AB489783A23FB26dA | evmos1fx944mzagwdhx0wz7k9tfztc8g3lkfk6rrgv6l
-	USER4_KEY="dev3"
-	USER4_MNEMONIC="doll midnight silk carpet brush boring pluck office gown inquiry duck chief aim exit gain never tennis crime fragile ship cloud surface exotic patch"
-
+	echo "VAL_KEY $VAL_KEY"
 	# Import keys from mnemonics
 	echo "$VAL_MNEMONIC" | evmosd keys add "$VAL_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
 
 	# Store the validator address in a variable to use it later
 	node_address=$(evmosd keys show -a "$VAL_KEY" --keyring-backend "$KEYRING" --home "$HOMEDIR")
 
-	echo "$USER1_MNEMONIC" | evmosd keys add "$USER1_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
-	echo "$USER2_MNEMONIC" | evmosd keys add "$USER2_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
-	echo "$USER3_MNEMONIC" | evmosd keys add "$USER3_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
-	echo "$USER4_MNEMONIC" | evmosd keys add "$USER4_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
-
 	# Set moniker and chain-id for Evmos (Moniker can be anything, chain-id must be an integer)
 	evmosd init $MONIKER --chain-id "$CHAINID" --home "$HOMEDIR"
 
-	# Testnet add-ons
-	# Fetch testnet related configs from evmos/testnets repo
-	# https://github.com/evmos/testnets/tree/main/evmos_9000-4
-
-	# Download and unzip the evmos_9000-4 testnet genesis.zip file
-	genesis_url="https://github.com/evmos/testnets/raw/main/evmos_9000-4/genesis.zip?download="
-	genesis_file="${HOMEDIR}/config/genesis.zip"
-	wget "$genesis_url" -O "$genesis_file"
-	unzip -o "$genesis_file" -d "${CONFIGDIR}"
+	# Download Genesis file for testnet evmos_9000-4
+#	wget -O ${GENESIS} https://archive.evmos.dev/evmos_9000-4/genesis.json
+	wget -O ${GENESIS} https://snapshots.polkachu.com/testnet-genesis/evmos/genesis.json
 
 	# Update persistent_peers and seeds for testnet setup
 	PEERS=`curl -sL https://raw.githubusercontent.com/evmos/testnets/main/evmos_9000-4/peers.txt | sort -r | head -n 10 | awk '{print $1}' | paste -s -d, -`
@@ -155,59 +106,6 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 
 	SEEDS=`curl -sL https://raw.githubusercontent.com/evmos/testnets/main/evmos_9000-4/seeds.txt | awk '{print $1}' | paste -s -d, -`
 	sed -i '/^seeds =/s/^/#/; /^#seeds =/a seeds = "'"$SEEDS"'"' ${CONFIG}
-	# End testnet add-ons
-
-	# Change parameter token denominations to aevmos
-	#	 - Uses sponge from moreutils package to minimze IO operations on large genesis file
-	#	   reads the standard input and writes it out to the specified file, to reduce the number of file operations:
-	# jq '.app_state["staking"]["params"]["bond_denom"]="aevmos"' "$GENESIS" | sponge "$GENESIS"
-	# jq '.app_state["crisis"]["constant_fee"]["denom"]="aevmos"' "$GENESIS" | sponge "$GENESIS"
-	# jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aevmos"' "$GENESIS" | sponge "$GENESIS"
-	# # When upgrade to cosmos-sdk v0.47, use gov.params to edit the deposit params
-	# jq '.app_state["gov"]["params"]["min_deposit"][0]["denom"]="aevmos"' "$GENESIS" | sponge "$GENESIS"
-	# jq '.app_state["evm"]["params"]["evm_denom"]="aevmos"' "$GENESIS" | sponge "$GENESIS"
-	# jq '.app_state["inflation"]["params"]["mint_denom"]="aevmos"' "$GENESIS" | sponge "$GENESIS"
-
-	# Set gas limit in genesis
-	# jq '.consensus_params["block"]["max_gas"]="10000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-	# Set claims start time
-	current_date=$(date -u +"%Y-%m-%dT%TZ")
-	# jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["airdrop_start_time"]=$current_date' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-	# Set claims records for validator account
-	# amount_to_claim=10000
-	# jq -r --arg node_address "$node_address" --arg amount_to_claim "$amount_to_claim" '.app_state["claims"]["claims_records"]=[{"initial_claimable_amount":$amount_to_claim, "actions_completed":[false, false, false, false],"address":$node_address}]' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-	# Set claims decay
-	# jq '.app_state["claims"]["params"]["duration_of_decay"]="1000000s"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-	# jq '.app_state["claims"]["params"]["duration_until_decay"]="100000s"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-	# Claim module account:
-	# 0xA61808Fe40fEb8B3433778BBC2ecECCAA47c8c47 || evmos15cvq3ljql6utxseh0zau9m8ve2j8erz89m5wkz
-	# jq -r --arg amount_to_claim "$amount_to_claim" '.app_state["bank"]["balances"] += [{"address":"evmos15cvq3ljql6utxseh0zau9m8ve2j8erz89m5wkz","coins":[{"denom":"aevmos", "amount":$amount_to_claim}]}]' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-	if [[ $1 == "pending" ]]; then
-		if [[ "$OSTYPE" == "darwin"* ]]; then
-			sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' "$CONFIG"
-			sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' "$CONFIG"
-			sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' "$CONFIG"
-			sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' "$CONFIG"
-			sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' "$CONFIG"
-			sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' "$CONFIG"
-			sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' "$CONFIG"
-			sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' "$CONFIG"
-		else
-			sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' "$CONFIG"
-			sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' "$CONFIG"
-			sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' "$CONFIG"
-			sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' "$CONFIG"
-			sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' "$CONFIG"
-			sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' "$CONFIG"
-			sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' "$CONFIG"
-			sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' "$CONFIG"
-		fi
-	fi
 
 	# enable prometheus metrics and all APIs for dev node
 	if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -226,61 +124,47 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 		grep -q -F '[memiavl]' "$APP_TOML" && sed -i '/\[memiavl\]/,/^\[/ s/enable = true/enable = false/' "$APP_TOML"
 	fi
 
-	# Change proposal periods to pass within a reasonable time for local testing
-	# sed -i.bak 's/"max_deposit_period": "172800s"/"max_deposit_period": "30s"/g' "$GENESIS"
-	# sed -i.bak 's/"voting_period": "172800s"/"voting_period": "30s"/g' "$GENESIS"
-
-	# set custom pruning settings
-	# sed -i.bak 's/pruning = "default"/pruning = "custom"/g' "$APP_TOML"
-	# sed -i.bak 's/pruning-keep-recent = "0"/pruning-keep-recent = "2"/g' "$APP_TOML"
-	# sed -i.bak 's/pruning-interval = "0"/pruning-interval = "10"/g' "$APP_TOML"
-
-	# Allocate genesis accounts (cosmos formatted addresses)
-	# evmosd add-genesis-account "$(evmosd keys show "$VAL_KEY" -a --keyring-backend "$KEYRING" --home "$HOMEDIR")" 100000000000000000000000000aevmos --keyring-backend "$KEYRING" --home "$HOMEDIR"
-	# evmosd add-genesis-account "$(evmosd keys show "$USER1_KEY" -a --keyring-backend "$KEYRING" --home "$HOMEDIR")" 1000000000000000000000aevmos --keyring-backend "$KEYRING" --home "$HOMEDIR"
-	# evmosd add-genesis-account "$(evmosd keys show "$USER2_KEY" -a --keyring-backend "$KEYRING" --home "$HOMEDIR")" 1000000000000000000000aevmos --keyring-backend "$KEYRING" --home "$HOMEDIR"
-	# evmosd add-genesis-account "$(evmosd keys show "$USER3_KEY" -a --keyring-backend "$KEYRING" --home "$HOMEDIR")" 1000000000000000000000aevmos --keyring-backend "$KEYRING" --home "$HOMEDIR"
-	# evmosd add-genesis-account "$(evmosd keys show "$USER4_KEY" -a --keyring-backend "$KEYRING" --home "$HOMEDIR")" 1000000000000000000000aevmos --keyring-backend "$KEYRING" --home "$HOMEDIR"
-
-	# bc is required to add these big numbers
-	# NOTE: we have the validator account (1e26) plus 4 (1e21) accounts
-	#       plus the claimed amount (1e4)
-	#total_supply=100004000000000000000010000
-	#jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-
-	# Sign genesis transaction
-	# echo "Sign genesis transaction"
-	# evmosd gentx "$VAL_KEY" 1000000000000000000000aevmos --keyring-backend "$KEYRING" --chain-id "$CHAINID" --home "$HOMEDIR"
-	## In case you want to create multiple validators at genesis
-	## 1. Back to `evmosd keys add` step, init more keys
-	## 2. Back to `evmosd add-genesis-account` step, add balance for those
-	## 3. Clone this ~/.evmosd home directory into some others, let's say `~/.clonedEvmosd`
-	## 4. Run `gentx` in each of those folders
-	## 5. Copy the `gentx-*` folders under `~/.clonedEvmosd/config/gentx/` folders into the original `~/.evmosd/config/gentx`
-
-	# Collect genesis tx
-	# echo "Collect genesis tx"
-	# evmosd collect-gentxs --home "$HOMEDIR"
-
-
 	# Edit genesys file in python to parse the JSON file in a streaming manner
 	# Less memory intensive
-	/usr/local/bin/genesis_edit.py $GENESIS
+	# echo "editing genesis"
+	# /usr/bin/genesis_edit.py $GENESIS
 
 	# Run this to ensure everything worked and that the genesis file is setup correctly
+	echo "validating genesis"
 	evmosd validate-genesis --home "$HOMEDIR"
-
-	if [[ $1 == "pending" ]]; then
-		echo "pending mode is on, please wait for the first block committed."
-	fi
 fi
 
+	echo "evmosd tx staking create-validator ..."
+	evmosd tx staking create-validator \
+	--amount=1000000atevmos \
+	--pubkey=$(evmosd tendermint show-validator) \
+	--moniker="${MONIKER}" \
+	--chain-id='evmos_9000-4' \
+	--commission-rate="0.05" \
+	--commission-max-rate="0.10" \
+	--commission-max-change-rate="0.01" \
+	--min-self-delegation="1000000" \
+	--gas="auto" \
+	--gas-prices="0.025atevmos" \
+	--from='mykey' \
+	--home="${HOMEDIR}"
+
+# evmosd tx staking create-validator \
+#   --amount=1000000000atevmos \
+#   --pubkey=$(evmosd tendermint show-validator) \
+#   --moniker="EvmosWhale" \
+#   --chain-id=evmos_9000-4 \
+#   --commission-rate="0.10" \
+#   --commission-max-rate="0.20" \
+#   --commission-max-change-rate="0.01" \
+#   --min-self-delegation="1000000" \
+#   --gas="auto" \
+#   --gas-prices="0.025atevmos" \
+#   --from='mykey'
+
 # Start the node
-# evmosd start \
-# 	--metrics "$TRACE" \
-# 	--log_level $LOGLEVEL \
-# 	--minimum-gas-prices=0.0001aevmos \
-# 	--json-rpc.api eth,txpool,personal,net,debug,web3 \
-# 	--home "$HOMEDIR" \
-# 	--chain-id "$CHAINID"
+evmosd start \
+	--metrics "$TRACE" \
+	--log_level $LOGLEVEL \
+	--json-rpc.api eth,txpool,personal,net,debug,web3 \
+	--home "$HOMEDIR"
